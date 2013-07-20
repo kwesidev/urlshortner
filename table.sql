7d2af1b5f4b2532d2e2e18b58934bcf6c0db8e3d
kwesidev <kwesidev@gmail.com>
2013-07-20 11:20:35 +0200

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `url_short`;
+CREATE TABLE `url_short` (
+  `code` text NOT NULL,
+  `longurl` text NOT NULL,
+  `created` int(10) NOT NULL,
+  PRIMARY KEY (`code`(100))
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
