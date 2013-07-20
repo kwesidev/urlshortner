d69307ec9b75aa8891eb503ec6c853dd09efd066
kwesidev <kwesidev@gmail.com>
2013-07-20 11:17:32 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,6 @@
+<?php
+//error_reporting(0);
+$conn = mysql_connect("yourhoust","yourusername","yourpassword") or die("<p>Cant connect try again later</p>");  
+    mysql_select_db("yourdatabasename",$conn);
+	  
+?>
