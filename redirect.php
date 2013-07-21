a92570faa8a0fb855368c143151faca0c7bb09c1
kwesidev <kwesidev@gmail.com>
2013-07-21 17:36:10 +0200

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -2,10 +2,11 @@
 //kwesidev@gmail.com
 include("database.php");
 if(isset($_GET['code'])){
-$code=$_GET['code'];
-  $url=mysql_fetch_array(mysql_query("SELECT * FROM url_short WHERE code='$code'",$conn));
-header("Location:".$url['longurl']);	
+    $code=$_GET['code'];
+    $query=mysql_query("SELECT * FROM url_short WHERE code='$code'",$conn);
+  $url=mysql_fetch_array($query);
+	if(mysql_num_rows($query)>0)
 
-}
-
+        header("Location:".$url['longurl']);	
+	   
 ?>
