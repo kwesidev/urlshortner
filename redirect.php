e2b71da293758b4377786bb25e71db5f7964964b
kwesidev <kwesidev@gmail.com>
2013-07-23 10:30:58 +0200

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -2,7 +2,7 @@
 //kwesidev@gmail.com
 include("database.php");
 if(isset($_GET['code'])){
-    $code=$_GET['code'];
+    $code=addslashes($_GET['code']);
     $query=mysql_query("SELECT * FROM url_short WHERE code='$code'",$conn);
   $url=mysql_fetch_array($query);
 	if(mysql_num_rows($query)>0)
