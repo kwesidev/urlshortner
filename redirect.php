4962fda483e8408eed60c1a1919443d588f3745d
kwesidev <kwesidev@gmail.com>
2013-07-27 18:40:40 +0200

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -4,9 +4,8 @@ include("database.php");
 if(isset($_GET['code'])){
     $code=addslashes($_GET['code']);
     $query=mysql_query("SELECT * FROM url_short WHERE code='$code'",$conn);
-  $url=mysql_fetch_array($query);
-	if(mysql_num_rows($query)>0)
-
-        header("Location:".$url['longurl']);	
+    $url=mysql_fetch_array($query);
+    if(mysql_num_rows($query)>0)
+                header("Location:".$url['longurl']);	
 	   
 ?>
