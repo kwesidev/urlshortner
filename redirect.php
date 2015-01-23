7a586869d5a6991ed32f2b7f07c1246804d2bae5
kwesidev <kwesidev@gmail.com>
2015-01-23 15:41:02 +0200

    assigned db_connect

--- redirect.php
+++ redirect.php
@@ -1,14 +1,11 @@
 <?php
 //redirect websites to original sites according to the code
-
 //make you put the .htaccess file in the current directory
-
 //(.*) index.php?code=$1;
-
 include("database.php");
 if(isset($_GET['code'])){
 $code=$_GET['code'];
-	
+$db=db_connect();	
 $row=$db->prepare("SELECT longurl from url_short where code=:code");
 $row->execute(array(":code"=>$code));
 $url=$row->fetch(PDO::FETCH_ASSOC);
