a06a3e1df605558daa9fcfcadea2a302bf9d0a30
kwesidev <kwesidev@gmail.com>
2015-01-23 15:38:06 +0200

    code refactoring ...OOP

--- index.php
+++ index.php
@@ -2,132 +2,17 @@
 //kwesidev@gmail.com
 //prevent site from blocking
 ini_set('user_agent',"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36");
-
-//kwesidev@gmail.com
-$start=time();
-$long="http://";
-include("database.php");
+include_once "database.php";
+include_once "UrlShortner.php";
 $message="";
-$original="";
-$url="yoururlshorterlink";
-$queryob="";
-
-//generate code
-function generate_code(){
-	
-//an array of alphabets and some characters
-$alpha=array_merge(range('a','z'),array('A','Z'));
-$code=$alpha[mt_rand(0,count($alpha)-1)].mt_rand(100,9000).substr((string)md5(time()),mt_rand(0,10),3);
-return($code);
-
-}
-
-
-function urlcheck($url){
-//checks for valid link
-$patte="/^http|https:\/\/[a-z0-9]+(\.[a-z0-9]+)+|(\/[a-zA-Z0-9\?\=\.]*)*$/";
-if(preg_match("/^http|https:\/\/(.*)+/",$url)) 
-	return(true);
-else
-	return(false);
-
-	
-}
-
-
-
-function checkcon($url){
-//checks if link exists
-
-  $fp=@fopen($url,"r"); 
- 
-	if($fp) 
-	return true;
-	else
-	
-     return false;
-	
-}
-
-function checkcode(&$obj){	  
-    
-
-   if($obj->rowCount()>0) 
- 
-   
-    return true;
-	else
-		
-		return false;
-}
-
+$start=time();
 if(isset($_REQUEST['act'])) {
-
-//gets longurl
-$long=urldecode($_REQUEST['longurl']);
-
-   $queryob=$db->prepare("SELECT * FROM url_short WHERE longurl=:url");
-   $queryob->execute(array("url"=>$long));
-   
-   
-if(urlcheck($long)){
-
-
-if(checkcode($queryob)){
-     $get_code=$queryob->fetch(PDO::FETCH_ASSOC);
-	 
-     $message="$url/".$get_code['code'];
-	 $queryob->closeCursor();	
-}
-
-
-
-else
-if(checkcon($long)){
-
-{
-
-$code=generate_code(); //generate code 
-
-//check to see if there are some duplicate code
-while(true){
-	
-$queryob=$db->prepare("SELECT * from url_short where code=:code");
-$queryob->execute(array(":code"=>$code));
-if(checkcode($queryob)==false)
-
-       break;	
-	   else
-	   $code=generate_code();
-}
-
-$tim=time();  
-
-//insert into db
-
-
-$queryob=$db->prepare("INSERT INTO url_short(code,longurl,created) values(:code,:longurl,:created,:access)");
-$queryob->execute(array("code"=>$code,"longurl"=>$long,"created"=>$tim));
-
-$message="$url/$code";
-}
-
-
-$queryob->closeCursor();
+	$long=urldecode($_POST['longurl']);
+	$shortner=new UrlShortner($long,$_POST['created']);
+	$message=$shortner->generateNewUrl();
+	$end=time();
 
 }
-
-else
-$message="Website does not exists";
-
-}
-
-else
-$message="Url is invalid";
-
-
-}
-$end=time();
 ?>
 <!DOCTYPE html>
 <html>
@@ -158,6 +43,7 @@ margin:auto;
   <p>
     <label>Type in Long URL</label>
     <input type="text" name="longurl" maxlength="1000" size="80" value="<?php  print @$long;?>"/>
+    <input type="hidden" name="created" value="<?php print time();?>" />
     <input type="submit" value="ShortenUrl"  name="act"/>
     
   </p>
