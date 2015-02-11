8b2188ec670bdb4cc1e95095128087032f01715a
kwesidev <kwesidev@gmail.com>
2015-02-11 17:35:25 +0200

    1)Added Comments to the functions 2)Code Indentaton

--- UrlShortner.php
+++ UrlShortner.php
@@ -19,101 +19,137 @@
  */
 
 class UrlShortner{
+   /**
+    *@var string
+   */
     private $webaddress;
+
+    /**
+     *@var long
+    */
     private $stamp;
+    /**
+     * Class Constructor
+     */
+    
     public function __construct($webaddress_,$stamp_){
 
-	    $this->webaddress=$webaddress_;
-    	$this->stamp=$stamp_;
-}
+	    $this->setWebAddress($webaddress_);
+    	    $this->setStamp($stamp);
+    }
 
+    /**
+     *wetsWebAddress
+     * @param String $webaddress_ actual webaddress to be Shortened
+     */
 
     public function setWebAddress($webaddress_){
 
 	    $this->webaddress=$webaddress_;
 
-}
-
+     }
+    /**
+     *Sets timestamp
+     *@param long $stamp_ unix stamp
+    */
     public function setStamp($stamp_){
 
 	    $this->stamp=$stamp_;
 
     }
+    /**
+     *Validates Url
+     *@return true | false
+    */
     private function validateUrl(){
          $pattern="/^http|https:\/\/[a-z0-9]+(\.[a-z0-9]+)+|(\/[a-zA-Z0-9\?\=\.]*)*$/";
-        if(preg_match("/^http|https:\/\/(.*)+/",$this->webaddress))
+         if(preg_match("/^http|https:\/\/(.*)+/",$this->webaddress))
             return(true);
-        else
+       	  else
             return(false);
 
-}
+    }
+
+    /**
+     *Generates random codes to be used in generating ShortUrls
+     *@return String 
+    */	
     private function generateCode(){
-//an array of alphabets and some characters
 	    $alpha=array_merge(range('a','z'),array('A','Z'));
-    	$code=$alpha[mt_rand(0,count($alpha)-1)].mt_rand(100,9000).substr((string)md5(time()),mt_rand(0,10),3);
+    	    $code=$alpha[mt_rand(0,count($alpha)-1)].mt_rand(100,9000).substr((string)md5(time()),mt_rand(0,10),3);
 	    return($code);
-}
+    }
+   /**
+    *Checks if Websites exists
+    *@return true|false
+   */
     private function checkSiteExists(){
         //checks if link exists
 
          $fp=@fopen($this->webaddress,"r"); 
  
     	if($fp) 
-		    return true;
-	    else
-    	 	return false;
-	}
+             return true;
+	else
+    	     return false;
+    }
+    /**
+     *Gets WebAddress
+     *@return String $webaddress_
+     */
     public function getWebAddress(){
 
 	    return($this->webaddress);
 
     }
+    /**
+     *Generates ShortUrl
+     *@return String ShortUrl
+    */
     public function generateNewUrl(){
       $message="";
-	
+      //Inner function for Checking for duplicates	
       function checkcode(&$obj){	  
     
 
-             if($obj->rowCount()>0) 
-                     return true;
-        	 else
-		
-	            	return false;
-}
-//gets longurl	
-   $db=db_connect();
-   $queryob=$db->prepare("SELECT * FROM url_short WHERE longurl=:url");
-   $queryob->execute(array("url"=>$this->webaddress));
-    if($this->validateUrl()){
-        if(checkcode($queryob)){
+          if($obj->rowCount()>0) 
+                return true;
+          else
+	        return false;
+    }
+     $db=db_connect();
+     $queryob=$db->prepare("SELECT * FROM url_short WHERE longurl=:url");
+     $queryob->execute(array("url"=>$this->webaddress));
+      if($this->validateUrl()){
+         if(checkcode($queryob)){
              $get_code=$queryob->fetch(PDO::FETCH_ASSOC);
              $message="http://short.url/".$get_code['code'];
-	         $queryob->closeCursor();	
-}
-else{ 
+	     $queryob->closeCursor();	
+        }
+      else{ 
  	if($this->checkSiteExists()){
 	        $new_code=$this->generateCode(); //generate code 
-//check to see if there are some duplicate code
+//checks if there are some duplicates codes if so ,generate new one
 	while(true){
 	
-		   $queryob=$db->prepare("SELECT * from url_short where code=:code");
-		   $queryob->execute(array(":code"=>$new_code));
+	       $queryob=$db->prepare("SELECT * from url_short where code=:code");
+	       $queryob->execute(array(":code"=>$new_code));
 	       if(checkcode($queryob)==false)
-       	         break;	
+       		         break;	
 	       else
 	  	        $new_code=$this->generateCode();
-}
+        }
 	$queryob=$db->prepare("INSERT INTO url_short(code,longurl,created) values(:code,:longurl,:created)");
 	$queryob->execute(array("code"=>$new_code,"longurl"=>$this->webaddress,"created"=>$this->stamp));
 	$message="http://short.url/".$new_code;
-}
+	}
 	else
 		$message="Website does not exists";
-}
-}
+	}
+	}
     else 
         $message="Invalid Url";
 
    return $message;
-}
+   }
 }
